34ddf6e35b42be57671d30ab65510a7a2453a42c
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-12-14 23:29:56 +0300

    chore(git): add openai

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ import { REST } from '@discordjs/rest';
 import Redis from 'ioredis';
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Client } from 'discord.js';
-
+import { Configuration, OpenAIApi } from 'openai';
 
 @Injectable()
 export class AppService implements OnApplicationBootstrap {
@@ -23,6 +23,20 @@ export class AppService implements OnApplicationBootstrap {
   }
 
   async test() {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
 
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
+      temperature: 0.5,
+      max_tokens: 60,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
+      stop: ["You:"],
+    });
   }
 }
