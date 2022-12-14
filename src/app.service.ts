b3ec840ede49cb3de7b6dd6014471c9b22e87f36
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-12-14 23:25:50 +0300

    feat(app): init app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,6 @@ import { MessageActionRowComponentBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import Redis from 'ioredis';
 import { InjectRedis } from '@nestjs-modules/ioredis';
-import { ChatGPTAPI, getOpenAIAuth } from 'chatgpt'
 import { Client } from 'discord.js';
 
 
@@ -24,22 +23,6 @@ export class AppService implements OnApplicationBootstrap {
   }
 
   async test() {
-     console.log(process.env.OPENAI_EMAIL, process.env.OPENAI_PASSWORD);
-    // use puppeteer to bypass cloudflare (headful because of captchas)
-    const openAIAuth = await getOpenAIAuth({
-      email: process.env.OPENAI_EMAIL,
-      password: process.env.OPENAI_PASSWORD
-    })
 
-    const api = new ChatGPTAPI({ ...openAIAuth })
-    await api.ensureAuth()
-
-    // send a message and wait for the response
-    const response = await api.sendMessage(
-      'Write a python version of bubble sort.'
-    )
-
-    // response is a markdown-formatted string
-    console.log(response)
   }
 }
