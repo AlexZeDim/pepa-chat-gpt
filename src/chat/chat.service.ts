be5730efc024630c8436b5d07828bbb1ad327abf
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2022-12-21 23:51:35 +0300

    fix(app): less context

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -195,7 +195,7 @@ export class ChatService {
 
   public whoAmIContext (authorName: string): string[] {
     return [
-      `${authorName}: Тебя зовут Пепа и ты любишь играть в World of Warcraft на монахе .`,
+      // `${authorName}: Тебя зовут Пепа и ты любишь играть в World of Warcraft на монахе .`,
       `Пепа: Меня зовут Пепа, я люблю играть в World of Warcraft на монахе.`,
       // `${authorName}: You like to push mythic plus, raid with friends and loot gear from weekly chests.`,
       // `Пепа: I like to push mythic plus, raid with friends and loot gear from weekly chest!`,
